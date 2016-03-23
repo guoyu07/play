9bae6a2c7e58dc88b8852cf3be2c8323612c378c
Ivan Jobs <ivan1377@163.com>
2016-03-23 18:31:52 +0800

    refine script for local vcenter ceph

--- ceph/s3/create_bucket.py
+++ ceph/s3/create_bucket.py
@@ -7,24 +7,30 @@ import hashlib
 import base64 
 import datetime
 
-bname = input() 
+host_port = '172.16.6.81:7480'
+#host_port = '10.192.40.29'
+bname = input('bname:\n') 
 # demouserid
 #access_key = 'Z2ETKC4RQFTR4XBQ1A72'
 #secret_key = 'vqdQGtmruGW855mduffA8lsLx+ot9iXIb9QTtT2I'
 
 #hr
-access_key = "9M3C3NCBEWSRDPRJGL0O"
-secret_key = "QCS0ju6dkqblLVQe966KwuE2Cg6cCfS/S2u2K+Qt"
+#access_key = "9M3C3NCBEWSRDPRJGL0O"
+#secret_key = "QCS0ju6dkqblLVQe966KwuE2Cg6cCfS/S2u2K+Qt"
 
 #eleme
 #access_key = 'VI8LSAC5JOFE99B066FC'
 #secret_key = 'm6ok1UbM+eTBqXXHRsAJ6PbUh3fmZDDfmOnHKk3M'
 
-req = Request('http://10.192.40.29/' + bname,
+# demo from local vcenter
+access_key = 'YG9YGNNYN46ARJH1MOEJ'
+secret_key = 'mxzTzqF7XZx00hmy7n4qzUQ5mKinYywuRD2xV4ka'
+
+req = Request('http://' + host_port +'/' + bname,
             method = 'PUT')
 timestr = datetime.datetime.utcnow().strftime('%a, %d %b %Y %H:%M:%S GMT')
 
-req.add_header('Host', '10.192.40.29')
+req.add_header('Host', host_port)
 req.add_header('Date', timestr)
 #req.add_header('x-amz-acl', 'public-read-write')
 
